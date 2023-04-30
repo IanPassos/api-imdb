4b0e7fa3ebf459b4823f9791aa1f407cc6e63913
Ian Rangel Passos <ianrangelpw@gmail.com>
2023-04-29 23:14:49 -0300

    Titles and image parsing with GSON

--- src/br/rc/imdb/main/model/accessApi.java
+++ src/br/rc/imdb/main/model/accessApi.java
@@ -7,15 +7,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.rc.imdb.main.utils.ConstantsEnum;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class accessApi {
 
 	public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
 		
 		
-		//System.out.println(ConstantsEnum.API_KEY.getValue());
+		
 		HttpRequest getRequest = HttpRequest.newBuilder().
 				uri(new URI("https://imdb-api.com/en/API/Top250Movies/" + ConstantsEnum.API_KEY.getValue()))
 				.GET()
@@ -27,8 +34,23 @@ public class accessApi {
 		
 		HttpResponse<String> getResponse = client.send(getRequest,BodyHandlers.ofString());
 		
-		System.out.println(getResponse.statusCode());
-		System.out.println(getResponse.body());
+		
+		String json = getResponse.body();
+		JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+		JsonArray moviesArray = jsonObject.getAsJsonArray("items");
+
+
+		List<String> titles = new ArrayList<>();
+		List<String> images = new ArrayList<>();
+		
+		for(int i = 0; i < moviesArray.size(); i++) {
+			String title = moviesArray.get(i).getAsJsonObject().get("title").getAsString();
+			titles.add(title);
+
+			String image = moviesArray.get(i).getAsJsonObject().get("image").getAsString();
+			images.add(image);
+		}
+
 	}
 	
 }
